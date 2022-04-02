2a6ccfd0fb67cd5fd3b413e0a20cc841a309507a
verowangari <verowangari34@gmail.com>
2022-04-03 01:19:44 +0300

    style login form

--- clone/urls.py
+++ clone/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as authViews 
 urlpatterns=[
-     path('', views.index, name='index'),
-     path('',views.login,name='login')
+    path('', views.index, name='index'),
+    # path('signup/', Signup, name='signup'),
+    # path('profile/edit', EditProfile, name='edit-profile'),
+    path('login/', authViews.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', authViews.LogoutView.as_view(), {'next_page' : 'index'}, name='logout'),
+   	# path('changepassword/', PasswordChange, name='change_password'),
+   	# path('changepassword/done', PasswordChangeDone, name='change_password_done'),
+   	path('passwordreset/', authViews.PasswordResetView.as_view(), name='password_reset'),
+   	path('passwordreset/done', authViews.PasswordResetDoneView.as_view(), name='password_reset_done'),
+   	path('passwordreset/<uidb64>/<token>/', authViews.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+   	path('passwordreset/complete/', authViews.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
