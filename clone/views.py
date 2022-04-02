159b44741227eec517f468716e44f5c317dcefa5
verowangari <verowangari34@gmail.com>
2022-04-03 00:32:52 +0300

    create base html template

--- clone/views.py
+++ clone/views.py
@@ -4,5 +4,5 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
-  template = loader.get_template('index.html')
+  template = loader.get_template('base.html')
   return HttpResponse(template.render())
