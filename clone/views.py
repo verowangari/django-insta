e2d26538ed37b3417ca92df3ba8fa54ec30cb440
verowangari <verowangari34@gmail.com>
2022-04-03 01:07:47 +0300

    add login form

--- clone/views.py
+++ clone/views.py
@@ -6,3 +6,8 @@ from django.template import loader
 def index(request):
   template = loader.get_template('base.html')
   return HttpResponse(template.render())
+
+
+def login(request):
+  template = loader.get_template('login.html')
+  return HttpResponse(template.render())
