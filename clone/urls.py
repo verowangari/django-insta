1b14335acb58e78da1d4a243126347ffd05ff5a1
verowangari <verowangari34@gmail.com>
2022-04-03 02:00:31 +0300

    create signup page

--- clone/urls.py
+++ clone/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as authViews 
+from clone.views import Signup
 urlpatterns=[
     path('', views.index, name='index'),
-    # path('signup/', Signup, name='signup'),
+    path('signup/', Signup, name='signup'),
     # path('profile/edit', EditProfile, name='edit-profile'),
     path('login/', authViews.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', authViews.LogoutView.as_view(), {'next_page' : 'index'}, name='logout'),
--- clone/views.py
+++ clone/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from django.template import loader
+from clone.forms import SignupForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -12,6 +14,20 @@ def login(request):
   template = loader.get_template('login.html')
   return HttpResponse(template.render())
 
-def signup(request):
-  template = loader.get_template('signup.html')
-  return HttpResponse(template.render())
+def Signup(request):
+	if request.method == 'POST':
+		form = SignupForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			email = form.cleaned_data.get('email')
+			password = form.cleaned_data.get('password')
+			User.objects.create_user(username=username, email=email, password=password)
+			return redirect('index')
+	else:
+		form = SignupForm()
+	
+	context = {
+		'form':form,
+	}
+
+	return render(request, 'signup.html', context)
