438ea2baf9aed64d35020584fe264a7c097ed906
verowangari <verowangari34@gmail.com>
2022-04-03 23:05:07 +0300

    create a form for uploading posts

---
+++ post/urls.py
@@ -0,0 +1,9 @@
+from post.views import home
+from django.urls import path
+
+urlpatterns=[
+    # path('',home,name='home'),
+    path('',home, name='home'),
+             
+             
+             ]
--- post/views.py
+++ post/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.template import loader
+from django.http import HttpResponse
 # Create your views here.
+from post.models import Post,Stream
+
+@login_required
+def home(request):
+    user=request.user
+    posts=Stream.objects.filter(user=user)
+    
+    group_ids=[]
+    
+    for post in posts:
+        group_ids.append(post.post_id)
+        
+    post_items = Post.objects.filter(id__in=group_ids).all().order_by('-posted')
+    
+    template=loader.get_template('home.html')
+    
+    context={
+        'post_items':post_items,
+    }
+    return HttpResponse(template.render(context,request))
+
+# def home(request):
+#   template = loader.get_template('home.html')
+#   return HttpResponse(template.render())
