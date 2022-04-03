bdedde9fcc87d55192b0e85a01fbb638a7a39e63
verowangari <verowangari34@gmail.com>
2022-04-03 23:44:51 +0300

    install django-notifications

--- post/urls.py
+++ post/urls.py
@@ -1,9 +1,10 @@
-from post.views import home
+from post.views import home, NewPost
 from django.urls import path
 
 urlpatterns=[
     # path('',home,name='home'),
     path('',home, name='home'),
+    path('newpost/',NewPost, name='newpost'),
              
              
              ]
