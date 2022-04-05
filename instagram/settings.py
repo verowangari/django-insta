fdf1bfafe1d712d6d5ffb40fc82a862623803d1f
verowangari <verowangari34@gmail.com>
2022-04-05 12:36:54 +0300

    correct deployment errors

--- instagram/settings.py
+++ instagram/settings.py
@@ -34,8 +34,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = '*'
-
+# ALLOWED_HOSTS = '*'
+ALLOWED_HOSTS = []
 
 
 # Application definition
