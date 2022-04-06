877910b8259570ffd8e921ce77aabbec9210be16
verowangari <verowangari34@gmail.com>
2022-04-06 12:02:02 +0300

    delete unused url

--- post/urls.py
+++ post/urls.py
@@ -1,20 +1,3 @@
-# from post.views import index,  NewPost,PostDetails,like
-# from django.urls import path
-
-# urlpatterns=[
-#     # path('',home,name='home'),
-#     # path('',home, name='home'),
-#     # path('',index,name='index'),
-#     path('newpost/',NewPost, name='newpost'),
-#     path('<uuid>:post_id',PostDetails, name='postdetails'),
-#     path('tag/<slug:tag_slug>',tags, name='tags'),
-#     path('<uuid>:post_id/like',like,name='postlike')
-             
-             
-#              ]
-
-
-
 from django.urls import path
 from . import views
 from django.conf import settings
