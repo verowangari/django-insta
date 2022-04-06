dbba612d24090535897bf5eebd182f86d2d7728e
verowangari <verowangari34@gmail.com>
2022-04-06 07:34:47 +0300

    correct identation

--- post/models.py
+++ post/models.py
@@ -112,9 +112,7 @@ class Likes(models.Model):
         like = instance
         post = like.post
         sender = like.user
-        # notify = Notification(post=post, sender=sender,
-        #                       user=post.user, notification_type=1)
-        # notify.save()
+    
 
     def user_unlike_post(sender, instance, *args, **kwargs):
         like = instance
